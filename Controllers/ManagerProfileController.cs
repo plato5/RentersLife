d2af82ba46590d5d5f9a6bed924845d31949651c
Cory Lewallen <lewallencory@gmail.com>
2022-03-01 11:53:33 -0800

    manager profile index

---
+++ Controllers/ManagerProfileController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RentersLife.Controllers
+{
+    public class ManagerProfileController : Controller
+    {
+        // GET: ManagerProfileController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ManagerProfileController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ManagerProfileController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ManagerProfileController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ManagerProfileController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ManagerProfileController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ManagerProfileController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: ManagerProfileController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
---
+++ Core/ViewModels/ImageViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RentersLife.Core.ViewModels
+{
+    public class ImageViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte[] Content { get; set; }
+    }
+}
