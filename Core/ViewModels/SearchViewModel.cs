37e88a99c9d1c948a7b8374b11d6c334cd853095
Cory Lewallen <lewallencory@gmail.com>
2022-04-10 21:00:14 -0700

    init search

---
+++ Core/ViewModels/SearchViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RentersLife.Core.ViewModels
+{
+    public class SearchViewModel
+    {
+        public string ZipCode { get; set; }
+        public int BedRooms { get; set; }
+        public int BathRooms { get; set; }
+        public double MinPrice { get; set; }
+        public double MaxPrice { get; set; }
+    }
+}
